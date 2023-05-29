b645578e784e94e1d8dd337b0f0b517cc5abf893
Alex Fomin <alexkillzone53@gmail.com>
2023-05-29 17:57:40 +1000

    Новые сущности для получения разметки от пользователя

---
+++ Core/Models/BooksImage.cs
@@ -0,0 +1,9 @@
+namespace Core.Models;
+
+public class BooksImage{
+
+
+    public Guid Id { get; set; }
+    public string Image { get; set; }
+    public List<Markup> Markups{ get; set; }
+}
---
+++ Core/Models/Markup.cs
@@ -0,0 +1,17 @@
+
+namespace Core.Models;
+
+public class Markup
+{
+    public Guid Id { get; set; }
+    //public string Image { get; set; }
+    public int x1 { get; set; }
+    public int x2 { get; set; }
+    public int x3 { get; set; }
+    public int x4 { get; set; }
+    public int y1 { get; set; }
+    public int y2 { get; set; }
+    public int y3 { get; set; }
+    public int y4 { get; set; }
+    public List<Text> Texts { get; set; }
+}
