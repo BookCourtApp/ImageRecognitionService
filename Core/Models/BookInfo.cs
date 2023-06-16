dace9967208f57c366281e86ab75ad9e16f6c375
Alex <alex@pop-os.localdomain>
2023-06-16 18:19:22 +1000

    Добавлены модели для ElasticSearch

---
+++ Core/Models/BookInfo.cs
@@ -0,0 +1,5 @@
+namespace Core.Models;
+public class BookInfo
+{
+    public string Info { get; set; }
+}
---
+++ Core/Models/ElasticResponse.cs
@@ -0,0 +1,7 @@
+namespace Core.Models;
+
+public class ElasticResponse
+{
+    public string BookInfo { get; set; }
+    public int EntryPoints { get; set; }
+}
